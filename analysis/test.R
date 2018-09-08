174ab1dc2f0529b3f4d7268df250ba5112fb8bd1
emrysf <1@fxcdev.com>
2018-09-08 22:10:47 +0800

    add analysis

--- analysis/test.R
+++ analysis/test.R
@@ -1,2 +1,15 @@
-st = 'test'
-print(st)
+setwd("/Users/emrys/Desktop")
+# Create a sequence of numbers between -10 and 10 incrementing by 0.1.
+x <- seq(-10, 10, by = .1)
+
+# Choose the mean as 2.5 and standard deviation as 0.5.
+y <- dnorm(x, mean = 2.5, sd = 0.5)
+
+# Give the chart file a name.
+png(file = "dnorm.png")
+
+plot(x,y)
+
+# Save the file.
+dev.off()
+
