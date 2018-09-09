5dc53c5b21a1cdd80862f9fdcdfb1a32bdef745b
emrysf <1@fxcdev.com>
2018-09-10 00:19:59 +0800

    [config] update R

--- analysis/test.R
+++ analysis/test.R
@@ -1,15 +1,20 @@
-setwd("/Users/emrys/Desktop")
-# Create a sequence of numbers between -10 and 10 incrementing by 0.1.
-x <- seq(-10, 10, by = .1)
+# Read the URL.
+url <- "http://www.geos.ed.ac.uk/~weather/jcmb_ws/"
 
-# Choose the mean as 2.5 and standard deviation as 0.5.
-y <- dnorm(x, mean = 2.5, sd = 0.5)
+# Gather the html links present in the webpage.
+links <- getHTMLLinks(url)
 
-# Give the chart file a name.
-png(file = "dnorm.png")
+# Identify only the links which point to the JCMB 2015 files. 
+filenames <- links[str_detect(links, "JCMB_2015")]
 
-plot(x,y)
+# Store the file names as a list.
+filenames_list <- as.list(filenames)
 
-# Save the file.
-dev.off()
+# Create a function to download the files by passing the URL and filename list.
+downloadcsv <- function (mainurl,filename) {
+  filedetails <- str_c(mainurl,filename)
+  download.file(filedetails,filename)
+}
 
+# Now apply the l_ply function and save the files into the current R working directory.
+l_ply(filenames,downloadcsv,mainurl = "http://www.geos.ed.ac.uk/~weather/jcmb_ws/")
