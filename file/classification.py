c3cc65f9ec650f60ec920ea5aba6ee42470c099e
emrysf <1@fxcdev.com>
2018-09-09 22:18:55 +0800

    [feature] update API doc

--- file/classification.py
+++ file/classification.py
@@ -2,20 +2,21 @@ import json
 import os
 import shutil
 
-src_path = '/Users/emrys/Documents/Moba/MobaBattleList'
-dest_dir = '/Users/emrys/Documents/bak_list'
+src_path = '/Users/emrys/Documents/Moba/MobaBattleDetail'
+dest_dir = '/Users/emrys/Documents/BattleDetail'
 
 
 def get_files(path):
     need_move_files = []
     for root, dirs, files in os.walk(path):
-        for file in files:
-            with open(os.path.join(path, file), 'r') as f:
-                if file == '.DS_Store':
-                    continue
-                j = json.loads(f.read())
-                if 'appitem' in j:
-                    need_move_files.append(file)
+        return files
+        # for file in files:
+            # with open(os.path.join(path, file), 'r') as f:
+            #     if file == '.DS_Store':
+            #         continue
+            #     j = json.loads(f.read())
+            #     if 'appitem' in j:
+            #         need_move_files.append(file)
 
     return need_move_files
 
@@ -28,6 +29,14 @@ def mv_files(files, dest_path):
         # shutil.move(file_p, )
 
 
+def cp_file(file, dest_path):
+    file_p = os.path.join(src_path, file)
+    dest_p = os.path.join(dest_path, file)
+    shutil.copy(file_p, dest_p)
+
+
 files = get_files(src_path)
 print(files)
+cp_file(files[0], dest_dir)
+# print(files)
 # mv_files(files, dest_dir)
