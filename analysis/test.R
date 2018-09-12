67c872c1cb8508598e8eaa78c26c8f05f0672d2a
emrysf <1@fxcdev.com>
2018-09-13 00:15:37 +0800

    [feature] add battle list hours and weeks analysis

--- analysis/test.R
+++ analysis/test.R
@@ -1,20 +1,37 @@
-# Read the URL.
-url <- "http://www.geos.ed.ac.uk/~weather/jcmb_ws/"
+setwd('/Users/emrys/Documents/Projects/MobaAnalysis/analysis')
 
-# Gather the html links present in the webpage.
-links <- getHTMLLinks(url)
+library('rjson')
 
-# Identify only the links which point to the JCMB 2015 files. 
-filenames <- links[str_detect(links, "JCMB_2015")]
-
-# Store the file names as a list.
-filenames_list <- as.list(filenames)
-
-# Create a function to download the files by passing the URL and filename list.
-downloadcsv <- function (mainurl,filename) {
-  filedetails <- str_c(mainurl,filename)
-  download.file(filedetails,filename)
-}
-
-# Now apply the l_ply function and save the files into the current R working directory.
-l_ply(filenames,downloadcsv,mainurl = "http://www.geos.ed.ac.uk/~weather/jcmb_ws/")
+result <- fromJSON(file="battle_hours.json")
+json_data = as.data.frame(result)
+print(json_data)
+print(typeof(json_data))
+b <- c(result['00'][1], 
+       result['01'][1], 
+       result['02'][1], 
+       result['03'][1], 
+       result['04'][1],
+       result['05'][1],
+       result['06'][1],
+       result['07'][1],
+       result['08'][1],
+       result['09'][1],
+       result['10'][1],
+       result['11'][1],
+       result['12'][1],
+       result['13'][1],
+       result['14'][1],
+       result['15'][1],
+       result['16'][1],
+       result['17'][1],
+       result['18'][1],
+       result['19'][1],
+       result['20'][1],
+       result['21'][1],
+       result['22'][1],
+       result['23'][1])
+v <- c(1, 2, 4)
+print(typeof(v))
+png(file='test.png')
+plot(json_data[0], type = 'o', main='Test')
+dev.off()
