756dbb157bc605211100dad3fc6f2442d620d7f0
emrysf <1@fxcdev.com>
2018-09-21 00:10:02 +0800

    [feature] add dump battle detail to csv

---
+++ file/dump_user_battle_data.py
@@ -0,0 +1,10 @@
+
+class UserBattleData:
+    def __init__(self):
+        self.spend_time = 0
+        self.record_day = set()
+        self.battle_count = 0
+
+
+def dump_user_battle_data():
+    pass
